96d01b5d55759630b8e5fa3016e39b5b2e55ddbd
icebear32 <67768810+icebear32@users.noreply.github.com>
2021-02-08 12:23:59 +0800

    third add files

---
+++ C1_LearnCLanguage/LC07_LoopStatement.c
@@ -0,0 +1,33 @@
+//c语言循环语句
+//    while循环
+//    do..while循环
+//    for循环
+
+//while循环案例
+//#include <stdio.h>
+//int main()
+//{
+//    printf("加入比特\n");
+//    int line = 0;
+//    while(line<=200)
+//   {
+//        line++;
+//        printf("我要继续努力敲代码\n");
+//   }
+//    if(line>200)
+//        printf("赢取白富美\n");
+//    return 0;
+//}
+
+//continue语句-->停止本次循环，继续下次循环
+//break语句-->终止循环，跳出循环
+
+
+/*
+
+循环语句三要素:
+    1.循环变量的初始化;
+    2.循环条件;
+    3.循环变量的更新语句
+
+*/
