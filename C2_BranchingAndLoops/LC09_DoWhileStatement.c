e056087715b8a62429aa766c999f0885ce8c155d
icebear32 <67768810+icebear32@users.noreply.github.com>
2021-02-12 22:48:34 +0800

    seventh add files

---
+++ C2_BranchingAndLoops/LC09_DoWhileStatement.c
@@ -0,0 +1,58 @@
+//do..while循环
+
+//	do语句的语法︰
+//		do
+//			循环语句; 
+//		while (表达式);
+
+//#include <stdio.h> 
+//
+//int main()
+//{
+//	int i =1;
+//
+//	do
+//	{
+//		printf("%d ", i);
+//		i++;
+//	} while (i <= 10);
+//
+//	return 0;
+//}
+
+
+
+////break
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	do
+//	{
+//		if (i == 5)
+//			break;
+//		printf("%d ", i);//打印0 1 2 3 4
+//		i++;
+//	} while (i <= 10);
+//
+//	return 0;
+//}
+
+
+//continue
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	do
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//		i++;
+//	} while (i <= 10);
+//
+//	return 0;
+//}
