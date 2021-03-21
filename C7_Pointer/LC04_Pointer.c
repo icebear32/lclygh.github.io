990a3d91b0f6f447f428f4500e9966ec2007e662
icebear32 <67768810+icebear32@users.noreply.github.com>
2021-03-21 17:25:10 +0800

    twenty-ninth add files

---
+++ C7_Pointer/LC04_Pointer.c
@@ -0,0 +1,56 @@
+//指针数组
+
+
+
+//#include <stdio.h>
+
+//int main()
+//{
+//	int arr[10] = { 0 };//整型数组
+//	char ch[5] = { 0 };//字符数组
+//	int* parr[4];//存放整形指针的数组–指针数组
+//	char* pch[5];//存放字符指针的数组–指针数组
+//
+//	return 0;
+//}
+
+
+//#include  <stdio.h>
+//
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 2,3,4,5,6 };
+//	int arr3[] = { 3,4,5,6,7 };
+//
+//	int* parr[] = { arr1,arr2,arr3 };
+//
+//	return 0;
+//}
+
+
+
+//遍历打印
+//#include  <stdio.h>
+//
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 2,3,4,5,6 };
+//	int arr3[] = { 3,4,5,6,7 };
+//
+//	int* parr[] = { arr1,arr2,arr3 };
+//
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 5; j++)
+//		{
+//			printf("%d", *(parr[i] + j));
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
